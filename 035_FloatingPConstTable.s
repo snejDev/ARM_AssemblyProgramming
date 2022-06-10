61928f8a127e826b89f37d6cca1bc6fcb04cd96b
Snehal Jayachander <66051560+snejDev@users.noreply.github.com>
2022-06-10 22:12:48 +0530

    Update 035_FloatingPConstTable.s

--- 035_FloatingPConstTable.s
+++ 035_FloatingPConstTable.s
@@ -1,17 +1,17 @@
 ;Constant table
-cpacr	equ		0xE00ED88
+cpacr		equ		0xE00ED88
 
 
-		area	prgm, code, readonly
+		area		prgm, code, readonly
 		entry
-		export 	__main
+		export 		__main
 			
 __main
 		;initialising the floating point co-processor
-		ldr			r0, =0xE00ED88
-		ldr			r1, [r0]
+		ldr		r0, =0xE00ED88
+		ldr		r1, [r0]
 		orr 		r1, r1, #(0xF<<20)
-		str			r1, [r0]
+		str		r1, [r0]
 		
 		adrl		r0,	constant_table
 		vldr.f32	s0,[r0]
@@ -22,11 +22,12 @@ stop 	b			stop
 		
 constant_table
 		dcd		0x3f800000	;1.000
-		dcd 	0x40000000	;2.0
+		dcd 		0x40000000	;2.0
 		dcd		0x80000000	;-0.0
 		dcd		0x41200000	;10.0
 		dcd		0x42C80000	;100.0
-		dcd 	0x4048F5C2  ;pi
-		dcd		0x402DF84C  ;e
+		dcd 		0x4048F5C2 	;pi
+		dcd		0x402DF84C  	;e
+		
 		align
 		end
